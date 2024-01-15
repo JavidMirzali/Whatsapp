86c6179a11bce498f03abf6394273914a2b6f37d
Javid Mirazli <javid.mirzeyev+demos@gmail.com>
2024-01-15 20:07:05 +0000

    demo

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 "use strict";
 $(document).ready(function () {
-
+    
     $("#me").click(function (e) {
         e.preventDefault();
         function myel(myclass = "") {
             if (!$.trim($("#inputme").val()) == "") {
                 let vall = $("#inputme").val()
                     , newH3 = $('<h3> </h3>')
-                    , newDiv = $('<div class = "line"> </div')
+                    , newDiv = $('<div class="line"> </div')
                     , bigDiv = $(`<div ${myclass}> </div`)
                 $(newH3).append(vall)
                 $(newDiv).append(newH3)
                 $(bigDiv).append(newDiv)
                 return bigDiv
-
             }
             else {
                 return
             }
-
         }
         $("#messageyou").append(myel())
-        $("#messageme").append(myel('class ="end"'))
+        $("#messageme").append(myel('class="end"'))
         $("#inputme").val("")
 
         document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
@@ -35,7 +33,7 @@ $(document).ready(function () {
             if (!$.trim($("#inputyou").val()) == "") {
                 let vall = $("#inputyou").val()
                     , newH3 = $('<h3> </h3>')
-                    , newDiv = $('<div class = "line"> </div')
+                    , newDiv = $('<div class="line"> </div')
                     , bigDiv = $(`<div ${myclass}> </div`)
                 $(newH3).append(vall)
                 $(newDiv).append(newH3)
@@ -47,7 +45,7 @@ $(document).ready(function () {
             }
         }
         $("#messageme").append(myel())
-        $("#messageyou").append(myel('class ="end"'))
+        $("#messageyou").append(myel('class="end"'))
         $("#inputyou").val("")
 
         document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
